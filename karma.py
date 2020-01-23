89263406392741418db1f86b39e6513cad547029
Chris Archibald <carchi8py@gmail.com>
2020-01-22 17:24:53 -0800

    add project

---
+++ karma.py
@@ -0,0 +1,22 @@
+import slack
+import time
+import logging
+from mytokem import token
+logging.basicConfig()
+slack_client = slack.WebClient(token=token)
+id = None
+mychannel = 'test-channel'
+list = slack_client.channels_list()
+for each in list.data.get('channels'):
+    if each.get('name') == mychannel:
+        id = each.get('id')
+while True:
+    response = slack_client.chat_postMessage(
+        channel='#' + mychannel,
+        text="This is a message")
+    if id:
+        l = slack_client.channels_history(channel=id)
+        print(str(l))
+        print('----------')
+    time.sleep(2)
+
